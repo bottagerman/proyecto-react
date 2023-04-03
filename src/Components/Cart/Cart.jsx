06b3be5ab2c1e032da4970e77891c52b89b4f529
bottagerman <botta.german@gmail.com>
2023-04-03 16:57:13 -0300

    Formulario y compra de productos

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,64 +1,90 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContex } from "../../context/CartContex";
 import Button from "@mui/material/Button";
 import CartMenu from "../CartMenu/CartMenu";
-import Swal from 'sweetalert2'
-
+import Swal from "sweetalert2";
+import FormCheckout from "../../FormCheckout/FormCheckout";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const { cart, clearCart, getTotalPrice } = useContext(CartContex);
 
   const precioTotal = getTotalPrice();
 
+  const [showForm, setShowForm] = useState(false);
+
+  const [orderId, setOrderId] = useState(null);
+
   const vaciarCarrito = () => {
     Swal.fire({
-      title: '¿Seguro desea eliminar estos productos?',
+      title: "¿Seguro desea eliminar estos productos?",
       showDenyButton: true,
       showCancelButton: true,
-      confirmButtonText: 'Eliminar',
+      confirmButtonText: "Eliminar",
       denyButtonText: `No elminar`,
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire('Carrito vaciado exitosamente', '', 'success')
-        clearCart()
+        Swal.fire("Carrito vaciado exitosamente", "", "success");
+        clearCart();
       } else if (result.isDenied) {
-        Swal.fire('El carrito no se vació', '', 'info')
+        Swal.fire("El carrito no se vació", "", "info");
       }
-    })
+    });
+  };
+
+  if(orderId){
+    return(
+      <div>
+        <h1>
+          Gracias por su compra! 
+        </h1>
+        <h3>
+          Su comprobante es: {orderId}
+        </h3>
+        <Link to="/"> Seguir comprando </Link>
+      </div>
+    )
   }
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      {cart.map((elemento) => {
-        return (
-          <div key={elemento.id}>
-            <CartMenu elemento={elemento} />
-          </div>
-        );
-      })}
-      <div style={{ display: "block" }}>
-        <h2> total carrito: ${precioTotal}</h2>
+    <div>
+      {" "}
+      {!showForm ? (
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {cart.map((elemento) => {
+            return (
+              <div key={elemento.id}>
+                <CartMenu elemento={elemento} />
+              </div>
+            );
+          })}
+          <div style={{ display: "block" }}>
+            <h2> total carrito: ${precioTotal}</h2>
 
-        {cart.length > 0 && (
-          <div>
-            <Button
-              onClick={vaciarCarrito}
-              variant="contained"
-              color="warning"
-              sx={{ mt: 20 }}
-            >
-              Vaciar Carrito
-            </Button>
-            <Button
-              variant="contained"
-              color="success"
-              sx={{ mt: 20, ml: "20px" }}
-            >
-              Comprar
-            </Button>
+            {cart.length > 0 && (
+              <div>
+                <Button
+                  onClick={vaciarCarrito}
+                  variant="contained"
+                  color="warning"
+                  sx={{ mt: 20 }}
+                >
+                  Vaciar Carrito
+                </Button>
+                <Button
+                  variant="contained"
+                  color="success"
+                  sx={{ mt: 20, ml: "20px" }}
+                  onClick={()=>setShowForm(true)}>
+                  Checkout
+                </Button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <FormCheckout cart={cart} getTotalPrice={getTotalPrice} setOrderId ={setOrderId} clearCart = {clearCart} />
+      )}
     </div>
   );
 };
--- src/FormCheckout/FormCheckout.jsx
+++ src/FormCheckout/FormCheckout.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { Button } from "@mui/material";
+import { addDoc, collection, updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
-const FormCheckout = () => {
+const FormCheckout = ({ cart, getTotalPrice, setOrderId, clearCart }) => {
+  const [userData, setUserData] = useState({
+    nombre: "",
+    email: "",
+    telefono: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let total = getTotalPrice();
+
+    let order = {
+      buyer: userData,
+      items: cart,
+      total,
+    };
+
+    let orderCollection = collection(db, "orders");
+    addDoc(orderCollection, order)
+      .then((res) => {
+        setOrderId(res.id);
+        clearCart();
+      })
+      .catch((err) => console.log(err));
+
+    cart.map((productos) => {
+      let refDoc = doc(db, "productos", productos.id);
+      updateDoc(refDoc, { stock: productos.stock - productos.quantity });
+    });
+  };
   return (
-    <div>FormCheckout</div>
-  )
-}
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 5, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nombre y apellido"
+          value={userData.nombre}
+          onChange={(e) => setUserData({ ...userData, nombre: e.target.value })}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          value={userData.email}
+          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        />
+        <TextField
+          required
+          id="outlined-number"
+          label="Telefono"
+          type="number"
+          value={userData.telefono}
+          onChange={(e) =>
+            setUserData({ ...userData, telefono: e.target.value })
+          }
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="success"
+          sx={{ mt: 5, ml: "20px" }}
+        >
+          Comprar
+        </Button>
+      </div>
+    </Box>
+  );
+};
 
-export default FormCheckout
+export default FormCheckout;
